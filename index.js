95ac9149744e2bc46f2d5f490e5cd40663783119
zhang-alvin <alv.zhang@gmail.com>
2020-05-31 23:23:14 -0400

    STATE: problem with getting download to finish

--- index.js
+++ index.js
@@ -6,7 +6,10 @@
 const fs = require("fs");
 const cp = require('child_process');
 const async = require('async');
-
+//const http = require('http');
+//onst https = require('https');
+const wget = require('wget-improved');
+const {once, EventEmitter} = require('events')
 
 function getFileNames(check){
 
@@ -28,6 +31,7 @@ function getFileNames(check){
     return fileURLs
 }
 
+/*
 function downloadFiles(url){ 
     const promise = new Promise(function(resolve,reject){
         cp.exec('wget -nH -x '+url+' -O dummy',
@@ -37,15 +41,105 @@ function downloadFiles(url){
                 }
             })
         })
-/*
-    cp.exec('wget -nH -x '+url+' -O dummy',
-            function (error, stdout, stderr) {
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
-            })
-*/
     return promise
+}
+*/
+
+//https://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js-without-using-third-party-libraries
+/* Doesn't provide full file download
+function downloadFiles(url, dest) {
+  var file = fs.createWriteStream(dest);
+  https.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close();
+    });
+  });
+}
+*/
+
+/*
+function needDeleteFile(dest){
+    fs.stat(dest,function(err,fileStat)    {
+        if(err){    
+            if (err.code == 'ENOENT') {
+                console.log('Does not exist.');
+            }       
+        }
+        else {
+            if (fileStat.isFile()) {
+                console.log('File found.');
+                fs.unlink(dest,function(err2){
+                    if(err2) throw err2;    
+                    console.log('Deletion successful')
+                });
+            }         
+        }
+    });
+}
+*/
+
+function needDeleteFile(dest){
+/*
+    fs.stat(dest,function(err,fileStat)    {
+        try{
+            if (fileStat.isFile()) {
+                console.log('File found.');
+                fs.unlink(dest,function(err2){
+                    if(err2) throw err2;    
+                    console.log('Deletion successful')
+                });
+            }         
+        }
+        catch(err){
+            if (err.code == 'ENOENT') {
+                console.log('Does not exist.');
+            }       
+        }
+        finally{
+            console.log('Proceeding')
+            return
+        }
+    });
+*/
+
+/*
+    try {
+        if (fs.existssync(dest)) {
+            //file exists
+            fs.unlink(dest,function(err2){
+                if(err2) throw err2;    
+                console.log('deletion successful')
+            });
+         }
+    } catch(err) {
+        continue
+    }
+*/
+    if (fs.existsSync(dest)) {
+        //file exists
+        fs.unlink(dest,function(err2){
+            if(err2) throw err2;    
+            console.log('deletion successful')
+        });
+    }
+
+}
+
+//from https://www.devdungeon.com/content/working-files-javascript-nodejs
+async function downloadFiles(url,dest){
+    needDeleteFile(dest);
+    let download = wget.download(url, dest);
+    console.log(download)
+    await once(download,'end')
+}
+
+function getFilesizeInBytes(filename) {
+    console.log("Here")
+    var stats = fs.statSync(filename)
+    console.log("THere")
+    var fileSizeInBytes = stats["size"]
+    return fileSizeInBytes
 }
 
 function getSizes(fileURLs){
@@ -53,20 +147,20 @@ function getSizes(fileURLs){
     var totalSize = 0
     for(url of fileURLs)
     {
+/*
         downloadFiles(url).then(
             totalSize+= getFilesizeInBytes('dummy'),
             console.log("total size is? "),
             console.log(totalSize),
         )
+*/
+        console.log("Fish")
+        let download = downloadFiles(url,'./dummy')
+        download.then(console.log("Papapya"))
+        totalSize+= getFilesizeInBytes('dummy')
+        console.log(totalSize)
     }
     return totalSize
-}
-
-async function getFilesizeInBytes(filename) {
-    var stats = await fs.statSync(filename)
-    var fileSizeInBytes = await stats["size"]
-    return fileSizeInBytes
-    //return  fs.statSync(filename)["size"]
 }
 
 module.exports = app => {
@@ -84,12 +178,24 @@ module.exports = app => {
     const repo = context.payload.repository.name
     const pull_number = context.payload.pull_request.number
 
-/*
     check = await context.github.pulls.listFiles({
         owner,repo,pull_number,
     });
-*/
+    console.log(check)
 
+    fileURLs=getFileNames(check);
+    console.log(fileURLs)
+
+    console.log("finished files");
+    totalSize=getSizes(fileURLs);
+    console.log(totalSize);
+    console.log("finished download");
+
+    //console.log('rm dummy')
+    //cp.exec('rm dummy')
+    //needDeleteFile('./dummy')
+
+/*
     totalSize =await async.waterfall([
         async function (callback){
             check = await context.github.pulls.listFiles({
@@ -119,15 +225,17 @@ module.exports = app => {
 
         return result 
     });
-
+    console.log(totalSize)
     if(context.payload.sender.login != 'sizecheck[bot]'){
         const issueComment = context.issue({ body: 'Apparent size is '+ totalSize/1024.0 + 'kB'})
         return context.github.issues.createComment(issueComment)
     }
+*/
   })
 
   //app.on(['pull_request.reopened','issue_comment.created'], async context => {
   //app.on('issue_comment.created', async context => {
+/*
   app.on('issue_comment.edited', async context => {
 
     if(context.payload.sender.login != 'sizecheck[bot]'){
@@ -189,6 +297,7 @@ module.exports = app => {
         return context.github.issues.createComment(issueComment)
     }
   })
+*/
 
 
   // For more information on building apps:
